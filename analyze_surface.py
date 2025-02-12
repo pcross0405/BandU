fixed bug in analyze surface causing color bar to have incorrect values, fixed bug in isosurface that prevented making surfaces at energy levels other than Fermi energy

analyze_surface.py
@@ -3,6 +3,8 @@ import pyvista as pv
 import numpy as np
 from isosurface import Isosurface
 from typing import Union
+from scipy.fft import fftn
+import matplotlib.pyplot as plt
 
 class AnalyzeSurface(Isosurface):
     def __init__(
@@ -30,7 +32,7 @@ class AnalyzeSurface(Isosurface):
         if show_endpoints:
             points = np.array([tail, shift+tail])
             points = pv.PolyData(points)
-            self.p.add_mesh(points.points, point_size=10, color='black', render_points_as_spheres=True)
+            self.p.add_mesh(points.points, point_size=20, color='black', render_points_as_spheres=True)
         self.p.add_mesh(py_arrow, color=color)
     #-----------------------------------------------------------------------------------------------------------------#
     # method to add reciprocal axes
@@ -177,25 +179,30 @@ class AnalyzeSurface(Isosurface):
             Cross section is a 2D slice instead of a section\n
             Default is to show cross section as 2D slice (True)
         '''
+        all_scalars = np.array([])
+        all_points = np.empty((1,3))
         # read isosurface save file
         with open(file_name, 'rb') as f:
-            bands = pkl.load(f)
+            bands:set = pkl.load(f)
             if type(opacity) == float:
                 opacity = np.ones(len(bands))*opacity            
-            bz_points = pkl.load(f)
-            rec_lattice = pkl.load(f)
+            bz_points:np.ndarray = pkl.load(f)
+            self.rec_lattice:np.ndarray = pkl.load(f)
             if BZ_show:
                 self.p.add_lines(bz_points, color='black', width=BZ_width)
             for i, _ in enumerate(bands):
-                iso_surf = pkl.load(f)
-                opacities = pkl.load(f)
-                scalars = pkl.load(f)
+                iso_surf:pv.PolyData = pkl.load(f)
+                opacities:np.ndarray = pkl.load(f)
+                scalars:np.ndarray = pkl.load(f)
+                scalars += 10**(-15)
+                all_scalars = np.append(all_scalars, scalars)
+                all_points = np.append(all_points, iso_surf.points, axis=0)
                 if cross_section != []:
                     opacities = self._CrossSection(
                         cross_section, 
                         iso_surf.points, 
                         cross_width, 
-                        rec_lattice, 
+                        self.rec_lattice, 
                         bz_points,
                         linear=linear,
                         two_dim=two_dim
@@ -215,19 +222,19 @@ class AnalyzeSurface(Isosurface):
                     roughness=roughness,
                     scalars=scalars,
                     cmap=colormap,
+                    clim=[np.min(scalars), np.max(scalars)],
                     opacity=opacities,
                     color=color,
                 )
-        # plot additional arrows as axes and/or nesting vectors
         if arrow != []:
-            self._AddArrow(arrow, rec_lattice, show_endpoints, arrow_color)
+            self._AddArrow(arrow, self.rec_lattice, show_endpoints, arrow_color)
         if periodic != []:
-            self._PeriodicArrow(arrow, periodic, rec_lattice, show_endpoints, arrow_color)
+            self._PeriodicArrow(arrow, periodic, self.rec_lattice, show_endpoints, arrow_color)
         if add_axes:
-            self._AddAxes(rec_lattice)
+            self._AddAxes(self.rec_lattice)
         if camera_position != []:
             camera_position = np.array(camera_position).reshape((3,3))
-            camera_position = np.matmul(camera_position, rec_lattice)
+            camera_position = np.matmul(camera_position, self.rec_lattice)
             self.p.camera_position = camera_position
         # render plotter
         self._Render()

isosurface.py
@@ -14,7 +14,7 @@ class Isosurface(WFK):
     )->None:
         if filename != '':
             super().__init__(filename)
-        self.p = BackgroundPlotter(window_size=(600,400))
+        self.p:pv.Plotter = BackgroundPlotter(window_size=(600,400))
         self.save = save
         self.save_file = save_file
         if save_file != 'Fermi_surface.pkl':
@@ -30,6 +30,8 @@ class Isosurface(WFK):
         # default energy is fermi energy
         if energy_level == None:
             energy_level = self.fermi
+        else:
+            energy_level += self.fermi
         # define upper and lower energy bounds
         min_nrg = energy_level - width/2
         max_nrg = energy_level + width/2
@@ -47,7 +49,7 @@ class Isosurface(WFK):
         band_num = set(band_num)
         return iso_pts, iso_values, band_num
     #-----------------------------------------------------------------------------------------------------------------#
-    # method for getting real space wavefunctions
+    # method for finding overlap value of BandU function with k-states
     def _FindOverlap(
             self, coeffs:np.ndarray, wfk_grid:np.ndarray, BandU:np.ndarray
     )->np.ndarray:
@@ -61,7 +63,7 @@ class Isosurface(WFK):
                 ngfft_grid[z][x][y] = wfk[i]
             ngfft_grid = fftn(ngfft_grid, norm='ortho')
             overlap = np.sum(np.conj(BandU)*ngfft_grid)
-            overlaps.append(np.abs(overlap))
+            overlaps.append(np.square(np.abs(overlap)))
         return np.array(overlaps).real
     #-----------------------------------------------------------------------------------------------------------------#
     # method getting eigenvalues and kpts within defined energy range, also get overlap of BandU eigfuncs w/ states
@@ -71,6 +73,8 @@ class Isosurface(WFK):
         # default energy is fermi energy
         if energy_level == None:
             energy_level = self.fermi
+        else:
+            energy_level = self.fermi + energy_level
         # define upper and lower energy bounds
         min_nrg = energy_level - width/2
         max_nrg = energy_level + width/2
@@ -263,7 +267,7 @@ class Isosurface(WFK):
             scalars:np.ndarray=None, overlap:np.ndarray=None, colormap:str='plasma', 
             hull:np.ndarray=None, delaunay:bool=True, lighting:bool=True, ambient:float=0.5, 
             diffuse:float=0.5, specular:float=1.0, specular_power:float=128.0, pbr:bool=False, 
-            metallic:float=0.5, roughness:float=0.5
+            metallic:float=0.5, roughness:float=0.5, energy_level:float=0.0
     )->None:
         # translate points to fill grid space
         if translation == None:
@@ -278,7 +282,7 @@ class Isosurface(WFK):
         trans_pts = pv.PolyData(trans_pts)
         trans_pts['values'] = trans_vals
         if null_value == None:
-                null_value = self.fermi+width/2*1.05
+                null_value = self.fermi+energy_level+width/2*1.05
         if scipy_interpolation:
             grid['values'] = self._ScipyInterpolate(
                 grid.points, 
@@ -297,7 +301,9 @@ class Isosurface(WFK):
         # create isosurface
         if rng == None:
             rng = [self.fermi, self.fermi]
-        iso_surf = grid.contour(isosurfaces=isosurfaces, rng=rng, method='contour')
+        else:
+            rng = [self.fermi + rng[0], self.fermi + rng[1]]
+        iso_surf:pv.PolyData = grid.contour(isosurfaces=isosurfaces, rng=rng, method='contour')
         opacities = self._SetOpacity(iso_surf.points, hull, delaunay)
         # apply Taubin smoothing
         if smooth:
@@ -344,7 +350,7 @@ class Isosurface(WFK):
                 cmap=colormap,
                 opacity=opacities,
                 color=color,
-                show_scalar_bar=False
+                show_scalar_bar=True
             )
     #-----------------------------------------------------------------------------------------------------------------#
     # method for getting isosurface color from BandU overlap with isosurface states
@@ -356,10 +362,10 @@ class Isosurface(WFK):
             real_func = np.zeros((self.ngfftz, self.ngfftx, self.ngffty), dtype=complex)
             imag_func = np.zeros((self.ngfftz, self.ngfftx, self.ngffty), dtype=complex)
             for num in xsf_nums:
-                real_path = xsf_root + f'_{num}_real.xsf'
+                real_path = xsf_root + f'_bandu_{num}_real.xsf'
                 real_xsf = XSF(real_path)
                 real_func += real_xsf.ReadDensity()
-                imag_path = xsf_root + f'_{num}_imag.xsf'
+                imag_path = xsf_root + f'_bandu_{num}_imag.xsf'
                 imag_xsf = XSF(imag_path)
                 imag_func += 1j*imag_xsf.ReadDensity()
             eigfunc = real_func + imag_func
@@ -369,9 +375,11 @@ class Isosurface(WFK):
             BandU_func = _ReadXSFs()
         # or calculate eigenfunction from WFK
         else:
-            raise NotImplementedError('''Currently this program only supports reading in density from an XSF file
-                                      Action: Set 'read_xsf' to True and provide 'xsf_path' with path to XSF file
-            ''')
+            raise NotImplementedError(
+                '''Currently this program only supports reading in density from an XSF file.
+                Action: Set 'read_xsf' to True and provide 'xsf_root' with the root name of the XSF file.
+                '''
+            )
             eigfuncs = []
             for eigfunc in self.BandU(energy_level=energy_level, width=width, states=states, XSFFormat=False):
                 eigfuncs.append(eigfunc)
@@ -520,7 +528,7 @@ class Isosurface(WFK):
                 scipy_interpolation=scipy_interpolation, color=color, overlap=overlap, hull=hull,
                 delaunay=delaunay, colormap=colormap, lighting=lighting, ambient=ambient, 
                 diffuse=diffuse, specular=specular, specular_power=specular_power, pbr=pbr, 
-                metallic=metallic, roughness=roughness
+                metallic=metallic, roughness=roughness, energy_level=energy_level
             )
         # render plot
         self._Render()