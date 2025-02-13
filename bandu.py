0d96eb5c39b50b57bf972cee85b32242a165c3e1
Petrock17 <boogee1715@gmail.com>
2025-02-13 16:44:00 -0600

    refactored bandu method to be its own object, now separate from general WFK object

---
+++ bandu.py
@@ -0,0 +1,116 @@
+import numpy as np
+from typing import Generator
+from wfk_class import WFK
+
+class BandU():
+    def __init__(
+        self, wfks:Generator, energy_level:float, width:float, grid:bool=True, fft:bool=True, norm:bool=True
+    )->None:
+        # find all states within width
+        self.bandu_fxns, total_states = self._FindStates(energy_level, width, wfks, grid, fft, norm)
+        # find overlap of states and diagonalize
+        principal_vals, principal_vecs = self._FindOverlap()
+        # linear combination of states weighted by principal components
+        self.bandu_fxns = np.matmul(principal_vecs, self.bandu_fxns)
+        # normalize bandu functions
+        for i in range(total_states):
+            normal_coeffs = WFK(wfk_coeffs=self.bandu_fxns[i,:])
+            normal_coeffs.Normalize()
+            self.bandu_fxns[i,:] = normal_coeffs.wfk_coeffs
+        # write output file
+        with open('eigenvalues.out', 'w') as f:
+            print(f'Width is {width}', file=f)
+            print(f'Calculated for states with energy {energy_level}', file=f)
+            print(principal_vals, file=f)
+    #-----------------------------------------------------------------------------------------------------------------#
+    # method transforming reciprocal space wfks to real space
+    def _FindStates(
+        self, energy_level:float, width:float, wfks:Generator, grid:bool, fft:bool, norm:bool
+    )->tuple[np.ndarray, int]:
+        # total number of states found within width
+        total_states = 0
+        # list for capturing all wfk coeffs of states within width
+        u_vecs = []
+        # state will be a WFK type
+        state:WFK
+        # find WFK type attributes, needed for writing XSF
+        for state in wfks:
+            self.ngfftx = state.ngfftx
+            self.ngffty = state.ngffty
+            self.ngfftz = state.ngfftz
+            self.lattice = state.lattice
+            self.natom = state.natom
+            self.typat = state.typat[0]
+            self.znucltypat = state.znucltypat
+            self.xred = state.xred
+            energy_level += state.fermi_energy
+            break
+        # loop through every state
+        for state in wfks:
+            # check if state has a band that crosses the width
+            for i, band in enumerate(state.eigenvalues):
+                if energy_level - width/2 <= band <= energy_level + width/2:
+                    total_states += 1
+                    # convert state to real space and add to u_vec list
+                    if grid:
+                        state = state.GridWFK(band_index=i)
+                    if fft:
+                        state = state.FFT()
+                    if norm:
+                        state = state.Normalize()
+                    u_vecs.append(state.wfk_coeffs)
+        # after FFT the wfk coefficients are reshaped from grid to vector for subsequent matrix operations
+        u_vecs = np.array(u_vecs).reshape(total_states, self.ngfftx*self.ngffty*self.ngfftz)
+        if total_states == 0:
+            raise ValueError(f'''Identified 0 states within provided width.
+            Action: Increase width or increase fineness of kpoint mesh.
+            ''')
+        return u_vecs, total_states
+    #-----------------------------------------------------------------------------------------------------------------#
+    # find overlap of states
+    def _FindOverlap(
+        self
+    )->tuple[np.ndarray, np.ndarray]:
+        # compute overlap matrix
+        print('Computing overlap matrix')
+        overlap_mat = np.matmul(np.conj(self.bandu_fxns), self.bandu_fxns.T)
+        # diagonlize matrix
+        principal_vals, principal_vecs = np.linalg.eig(overlap_mat)
+        # organize eigenvectors and eigenvalues
+        sorted_inds = np.flip(principal_vals.argsort())
+        principal_vecs = principal_vecs.T
+        principal_vals = np.take(principal_vals, sorted_inds)
+        principal_vecs = np.take(principal_vecs, sorted_inds, axis=0)
+        return principal_vals, principal_vecs
+    #-----------------------------------------------------------------------------------------------------------------#
+    # make xsf of BandU functions
+    def MakeXSF(
+        self, function_number:list, xsf_file:str
+    )->None:
+        if len(function_number) != 2:
+            raise ValueError(f'function_number should contain two variables, {len(function_number)} were received.')
+        count = 1
+        while function_number[0] <= count <= function_number[1]:
+            # fetch nth bandu function coefficients
+            bandu_fxn = self.bandu_fxns[count-1,:]
+            # grid coefficients into 3D numpy array
+            bandu_fxn = bandu_fxn.reshape((self.ngfftz, self.ngfftx, self.ngffty))
+            # create WFK object from coefficients and provided attributes
+            bandu_fxn = WFK(
+                wfk_coeffs=bandu_fxn, 
+                lattice=self.lattice,
+                natom=self.natom,
+                xred=self.xred,
+                typat=self.typat,
+                znucltypat=self.znucltypat,
+                ngfftx=self.ngfftx,
+                ngffty=self.ngffty,
+                ngfftz=self.ngfftz
+            )
+            # convert to XSF format
+            bandu_fxn = bandu_fxn.XSFFormat()
+            # print out XSF file
+            xsf_name = xsf_file + f'_{count}'
+            bandu_fxn.WriteXSF(xsf_file=xsf_name)
+            # update count and move to next bandu function
+            count += 1
